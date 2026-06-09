e34248ca6adf19eeb8957b1390523b3d797d5a7a
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-06-09 15:40:21 +1000

    Implement get and post endpoints

--- ingredient-service/server/api/index.get.ts
+++ ingredient-service/server/api/index.get.ts
@@ -1,5 +1,12 @@
-import { defineHandler } from "nitro"
+import { defineHandler } from "nitro";
+import pool from '../database';
+import type { RowDataPacket, FieldPacket } from "mysql2/promise";
+import Ingredient from "#server/entity/Ingredient.ts";
 
-export default defineHandler((event) => {
-  return { message: "All ingredient retrieved successfully" };
+export default defineHandler(async (event) => {
+
+    const [rows, _fields]: [RowDataPacket[], FieldPacket[]] = await pool.execute('SELECT * FROM ingredients');
+    const ingredients = rows.map((row: RowDataPacket) => new Ingredient(row.id, row.name, row.quantity, row.unit, row.category));
+
+    return { data: ingredients };
 });
