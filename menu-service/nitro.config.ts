c8a0a818abe52c565613ec78db71769561cb5dde
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-06-11 14:13:26 +1000

    Setup the run time config

--- menu-service/nitro.config.ts
+++ menu-service/nitro.config.ts
@@ -2,4 +2,13 @@ import { defineConfig } from "nitro"
 
 export default defineConfig({
   serverDir: './server',
+  runtimeConfig: {
+    mysql: {
+      host: process.env.MYSQL_HOST || 'localhost',
+      port: Number(process.env.MYSQL_PORT || 3306),
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+    },
+  }
 });
